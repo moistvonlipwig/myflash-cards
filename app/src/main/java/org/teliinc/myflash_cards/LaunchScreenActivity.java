8935306068e4c033675c32d56d796fd5e81d69a8
chris.teli@gmail.com <chris.teli@gmail.com>
2015-12-08 00:05:38 -0800

    Fixed the intial load using a progress dialog.

--- app/src/main/java/org/teliinc/myflash_cards/LaunchScreenActivity.java
+++ app/src/main/java/org/teliinc/myflash_cards/LaunchScreenActivity.java
@@ -1,9 +1,13 @@
 package org.teliinc.myflash_cards;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.Handler;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.view.View;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -11,65 +15,98 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class LaunchScreenActivity extends AppCompatActivity {
 
-    // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    // ProgressBar for loading data
+    private ProgressDialog progress;
+
+    // Flashcard Reference
+    Firebase flashcardRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
 
-        // TODO : Use AsyncTask to retrieve data and display progress bar
+
         // Setup Firebase
         Firebase.setAndroidContext(this);
 
-        // Read the data and store in FlashCards
-        Firebase flashcardRef = new Firebase("https://teliflashcards.firebaseio.com/FlashCards/");
+        // Initialized the ProgressDialog and Start download
+        progress=new ProgressDialog(this);
+        progress.setMessage("Downloading FlashCards");
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setIndeterminate(true);
+        progress.setProgress(0);
+        progress.show();
 
-        FlashCard.RetreivedFlashCards =  new ArrayList<>();
-        // Attach an listener to read the data at our posts reference
-        flashcardRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                // Iterate through the list and store in list
-                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                    Map<String,String> map = (Map<String,String>)postSnapshot.getValue();
-                    FlashCard f = new FlashCard(map.get("answer"),map.get("question"));
+        // Download in AsyncTask
+        new AsyncDataLoad().execute(0);
+    }
 
-                    // TODO : Get Tags and update RetreivedFlashCards
-                    FlashCard.RetreivedFlashCards.add(f);
+    class AsyncDataLoad extends AsyncTask<Integer, Integer, String> {
+
+        // Initialize the progressbar
+        protected void onPreExecute() {
+            //do initialization of required objects objects here
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Integer... Params) {
+
+            // Get Flashcard Ref
+            flashcardRef = new Firebase("https://teliflashcards.firebaseio.com/FlashCards/");
+
+            // Initialize FlashCard List
+            // List will be populated from database
+            FlashCard.RetreivedFlashCards = new ArrayList<>();
+
+            // Read the data and store in FlashCards
+            flashcardRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    // Iterate through the list and store in list
+                    int count = 0;
+                    progress.setMax((int) snapshot.getChildrenCount());
+                    for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                        publishProgress(count++);
+                        Map<String, String> map = (Map<String, String>) postSnapshot.getValue();
+                        FlashCard f = new FlashCard(map.get("answer"), map.get("question"));
+
+                        // TODO : Get Tags and update RetreivedFlashCards
+                        FlashCard.RetreivedFlashCards.add(f);
+                    }
                 }
-            }
 
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-                System.out.println("The read failed: " + firebaseError.getMessage());
-            }
-        });
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+                    System.out.println("The read failed: " + firebaseError.getMessage());
+                }
+
+            });
+            return "Tast Completed";
+        }
 
 
-        new Handler().postDelayed(new Runnable() {
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progress.dismiss();
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            startActivity(intent);
+            // close this activity
+            finish();
+        }
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(LaunchScreenActivity.this, MainActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress.setProgress(1);
+            Log.i("ProgressUpdate", "Updating" + values[0]);
+        }
     }
 }
+
+
