1eb279c5a5abcc9734bdb00d7812c94e702c8043
chris.teli@gmail.com <chris.teli@gmail.com>
2015-12-14 22:44:11 -0800

    Final Beta Version

--- app/src/main/java/org/teliinc/myflash_cards/RecyclierViewHelpers/RecyclerItemClickListener.java
+++ app/src/main/java/org/teliinc/myflash_cards/RecyclierViewHelpers/RecyclerItemClickListener.java
@@ -3,8 +3,13 @@ package org.teliinc.myflash_cards.RecyclierViewHelpers;
 /**
  * Created by cteli on 12/12/2015.
  */
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -12,11 +17,13 @@ import android.view.View;
 
 import org.teliinc.myflash_cards.Activties.DisplayCardActivity;
 import org.teliinc.myflash_cards.Model.FlashCard;
+import org.teliinc.myflash_cards.Model.FlashCardTag;
 
 import java.util.ArrayList;
 
 public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
     private OnItemClickListener mListener;
+    private Context context;
 
     public interface OnItemClickListener {
         public void onItemClick(View view, int position);
@@ -25,6 +32,7 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
     GestureDetector mGestureDetector;
 
     public RecyclerItemClickListener(Context context, OnItemClickListener listener) {
+        this.context = context;
         mListener = listener;
         mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
@@ -38,14 +46,25 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
     public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
         View childView = view.findChildViewUnder(e.getX(), e.getY());
         int position = view.getChildAdapterPosition(childView);
-        if (childView != null ) {
-                Intent intent = new Intent(view.getContext(), DisplayCardActivity.class);
+        if (childView != null) {
+            // Check Preferences
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(view.getContext());
+            int browseType = Integer.parseInt(sharedPref.getString("pref_browseType", "0"));
+
+            Intent intent = new Intent(view.getContext(), DisplayCardActivity.class);
+            Bundle extras = ((Activity)context).getIntent().getExtras();
+            if(browseType==0) {
                 intent.putExtra("QUESTION", FlashCard.RetreivedFlashCards.get(position).getQuestion());
                 intent.putExtra("ANSWER", FlashCard.RetreivedFlashCards.get(position).getAnswer());
-                ArrayList<String> tagsList = new ArrayList<>();
-                tagsList.addAll(FlashCard.RetreivedFlashCards.get(position).getTags());
-                intent.putStringArrayListExtra("TAGS", tagsList);
-                view.getContext().startActivity(intent);
+            } else {
+                String tag = extras.getString("TAG");
+                intent.putExtra("QUESTION", FlashCardTag.tagQuestions.get(tag).RetreivedQuestions.get(position).getQuestion());
+                intent.putExtra("ANSWER", FlashCardTag.tagQuestions.get(tag).RetreivedQuestions.get(position).getAnswer());
+            }
+            ArrayList<String> tagsList = new ArrayList<>();
+            tagsList.addAll(FlashCard.RetreivedFlashCards.get(position).getTags());
+            intent.putStringArrayListExtra("TAGS", tagsList);
+            view.getContext().startActivity(intent);
         }
         return false;
     }
